3e4f72df5c18d154a98b57a9bbc060f3f3527def
Anthony Williams <hi@antw.me>
2010-08-12 15:34:00 +0100

    Add Python binary path, and always use /usr/local over /usr.

--- lib/paths.zsh
+++ lib/paths.zsh
@@ -1,2 +1,4 @@
-PATH="$HOME/bin:$PATH"; export PATH
+# Make binaries in ~/bin available, and always use /usr/local/bin
+# in preference over /usr/bin.
+PATH="$HOME/bin:/usr/local/bin:/usr/local/Cellar/python/2.7/bin:$PATH"; export PATH
 
